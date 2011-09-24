0d1fc56755ae06560c369fbdea0ed8db7878a0b3
James Polley <jamezpolley@gmail.com>
2011-09-24 18:55:59 +1000

    Workings.

--- list_tasks.py
+++ list_tasks.py
@@ -1,35 +1,37 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 """ wunderlist-display Looks up the projects and tasks in your wunderlist.db.
 Exports them in a ANSI marked up manner appropriate for display in geektool3.
 Based on code from Matthew Cave https://github.com/ender3/wunderlist-geektool
 """
 
 import datetime
+import getpass
 import sqlite3
 
-USERNAME="james"
+USERNAME=getpass.getuser()
 
-conn = sqlite3.connect("/Users/%s/Library/Application Support/Titanium/"
-        "appdata/com.wunderkinder.wunderlist/wunderlist.db" % USERNAME)
+db_filename = ("/Users/%s/Library/Wunderlist/wunderlist.db" % USERNAME)
+conn = sqlite3.connect(db_filename)
 conn.row_factory = sqlite3.Row
 
-ESCAPE_IMPORTANT_COLOR = "\033[33m" # defaults to yellow
-ESCAPE_PAST_DUE_COLOR = "\033[31m" # defaults to red
-ESCAPE_PROJECT_NAME_MARKUP = "\033[1m" # defauls to bold
-ESCAPE_NOTE_MARKUP = "\033[3m" # defaults to italic
-ESCAPE_CANCEL = "\033[0m" # resets all text atributes you may have set
-INDENT = "    "
+ESCAPE_IMPORTANT_COLOR = u"\033[33m" # defaults to yellow
+ESCAPE_PAST_DUE_COLOR = u"\033[31m" # defaults to red
+ESCAPE_PROJECT_NAME_MARKUP = u"\033[1m" # defauls to bold
+ESCAPE_NOTE_MARKUP = u"\033[3m" # defaults to italic
+ESCAPE_CANCEL = u"\033[0m" # resets all text atributes you may have set
+INDENT = u"    "
 
 
 list_cursor = conn.cursor()
-lists = cursor.execute("select id, name, position from lists where deleted = '0' order by position")
+lists = list_cursor.execute("select id, name, position from lists where deleted = '0' order by position")
 for list in lists:
     printedHeader = False
     task_cursor = conn.cursor()
     tasks = task_cursor.execute("select id, date, name, note, important, "
             "position from tasks where deleted = 0 and done = 0 and "
-            "list_id = ? order by date DESC, important DESC, position",
-            list["id"])
+            "list_id = ? order by date ASC, important DESC, position",
+            str(list["id"]))
 
     for task in tasks:
         # There is a task associated with this list, so print the header
@@ -41,25 +43,24 @@ for list in lists:
         taskToOutput = []
         # if there is a date, print it before the name of the task
         if (task['date'] != 0 ):
-            date = datetime.date(task['date'])
-            today = datetime.date(datetime.date.today())
+            date = datetime.date.fromtimestamp(task['date'])
+            today = datetime.date.today()
 
             #if the date is in the past, print the date in red
             if (today > date):
-                taskToOutput.append(INDENT + ESCAPE_PAST_DUE_COLOR)
-                taskToOutput.append(date.month + "/" + date.day + "/")
-                taskToOutput.append(date.year + ESCAPE_CANCEL + " - ")
+                taskToOutput.extend((INDENT, ESCAPE_PAST_DUE_COLOR, 
+                    unicode(date.strftime('%x')), ESCAPE_CANCEL, u" - "))
             else:
-                taskToOutput.append(INDENT + date.month + "/" + date.day)
-                taskToOutput.append("/" + date.year.to_s + " - ")
+                taskToOutput.extend((INDENT, unicode(date.strftime('%x')),
+                    u" - "))
         else:
             taskToOutput.append(INDENT)
 
         if (task['important'] == 1):
-          taskToOutput.append(ESCAPE_IMPORTANT_COLOR + "★ " + ESCAPE_CANCEL)
-        taskToOutput.append(task['name'])
+          taskToOutput.extend((ESCAPE_IMPORTANT_COLOR, u"★ ", ESCAPE_CANCEL))
+        taskToOutput.extend((task['name']))
 
-        print ''.join(taskToOutput)
+        print ''.join(unicode(x) for x in taskToOutput)
 
         # if there is a note, print it in italics
         if task['note']:
